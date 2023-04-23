73e3502d5203ec9746b9c9242d8c169e32a906f4
aman-av <itz.aman.av@gmail.com>
2023-04-24 04:32:20 +0530

    archieved jobs feature added

--- routes/jobsroute.js
+++ routes/jobsroute.js
@@ -1,6 +1,9 @@
+const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 require("../model/jobschema");
-require('../model/jobsorder')
+require('../model/archievedjobs');
+require('../model/jobsorder');
+const Archivedjobs = mongoose.model('archievedjobs')
 const Jobsorder = mongoose.model('jobsorders');
 const Jobs = mongoose.model("jobs");
 
@@ -32,10 +35,38 @@ module.exports = async(app) => {
 
     app.get('/jobs/alljobs', async (req, res) => {
         const response = await Jobs.find({});
-        const dict = {}
+//         For simplicity, the listing which
+// are due in more than 21 days should be green, less than 14
+// days should be yellow and less than 3 days should be red
+        const curdate = new Date()
+        var dict = {}
         response.map(item => {
-            dict[item._id]=item
-        })
+            dict[item._id] = {...item['_doc']};
+            let diff = new Date(item.deadline) - curdate;
+            var diffDays = parseInt((diff) / (1000 * 60 * 60 * 24), 10);
+            if (diffDays > 21)
+                dict[item._id].color = 'lightgreen';
+            else if (diffDays > 14)
+                dict[item._id].color = 'yellow';
+            else if (diffDays >=3)
+                dict[item._id].color = 'orange';
+            else if (diffDays < 3 && diffDays >= 0)
+                dict[item._id].color = 'red';
+            else if (diffDays < 0) {
+                const func = async () => {
+                    
+                    const resp = await Jobs.findOneAndDelete({ _id: ObjectId(item._id) });
+
+                    const user = new Archivedjobs(resp);
+
+                    await user.save();
+
+                }
+                func();
+            }
+                console.log(dict)
+        });
+        console.log(dict);
         console.log(dict);
 
 //         await Jobsorder.updateOne( {key:'7984dsfdsf'}, {
@@ -47,7 +78,17 @@ module.exports = async(app) => {
     });
 
     app.get('/jobs/getorder', async (req, res) => {
-          const response = await Jobsorder.find({});
+        const response = await Jobsorder.find({});
         res.send(response[0]['listorder']);
-    })
+    });
+
+    app.post('/jobs/updateorder', async (req, res) => {
+        // const response = await Jobsorder.find({});
+
+        const response = await Jobsorder.updateOne({ key: '7984dsfdsf' },
+            {
+                $set: { "listorder": req.body }
+            })
+        res.send(response);
+    });
 }
