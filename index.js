2d86b8e80559daf675f46a06396f464068878cb7
aman-av <55616539+aman-av@users.noreply.github.com>
2023-05-01 18:03:53 +0530

    update index.js

--- index.js
+++ index.js
@@ -50,12 +50,7 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(
-	  	cors({
-			    origin: ["http://localhost:3000",'https://nybulajobs.netlify.app/'], 
-		    	methods: [ "GET", "POST","DELETE" ],
-		    	credentials: true,
-		  	})
-		); 
+	  	cors(); 
 
 app.get('/', (req, res) => {
     res.json({
